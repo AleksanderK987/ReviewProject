dca70c09994301aebe47e144854096e0c399e3e6
Aleksander Kot <akot522@gmail.com>
2024-06-14 10:13:42 +0200

    small fixes

--- js/javascript.js
+++ js/javascript.js
@@ -1,15 +1,10 @@
 //function to send reviews to database
 function sendReview() {
-    const form = document.getElementById('form');
-    const formData = new FormData(form);
 
     fetch('php/sending.php', {
         method: 'POST',
-        body: formData
     })
-    .then(response => response.json())
     .then(data => {
-        const responseDiv = document.getElementById('response');
         if (data.status === 'success') {
             window.location.reload();
         } else {
@@ -26,7 +21,6 @@ function deleteReview(id){
     fetch(`../php/delete_records.php?id=${id}`, {
         method: 'POST',
     })
-    .then(response => response.json())
     .then(data => {
         if (data.status === 'success') {
             window.location.reload();
